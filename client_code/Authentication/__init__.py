d58af097142ed09252e21c925bb7213596290dc0
sule4835@kettering.edu <sule4835@kettering.edu>
2021-05-19 17:42:46 +0000

    Created ServerModule1 and Authentication; Edited settings; Deleted RolePage, LoanOfficer, Student and Registrar

--- client_code/Authentication/__init__.py
+++ client_code/Authentication/__init__.py
@@ -1,11 +1,13 @@
-from ._anvil_designer import RegistrarTemplate
+from ._anvil_designer import AuthenticationTemplate
 from anvil import *
+import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import custom_signup.login_flow
 
-class Registrar(RegistrarTemplate):
+class Authentication(AuthenticationTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
