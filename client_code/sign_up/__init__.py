b1b15f18c40f4aeece172e1474593e85ce87bc04
sule4835@kettering.edu <sule4835@kettering.edu>
2021-05-19 18:02:11 +0000

    Created sign_up; Edited settings; Deleted Authentication

--- client_code/sign_up/__init__.py
+++ client_code/sign_up/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import AuthenticationTemplate
+from ._anvil_designer import sign_upTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,10 +7,15 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import custom_signup.login_flow
 
-class Authentication(AuthenticationTemplate):
+class sign_up(sign_upTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
     
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
