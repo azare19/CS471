10483838a17bda8c7a70957cd7036e631f75e7e4
sule4835@kettering.edu <sule4835@kettering.edu>
2021-05-26 17:48:29 +0000

    Created harold.jpg, welcome_page and sign_in; Edited settings; Deleted Form1

--- client_code/welcome_page/__init__.py
+++ client_code/welcome_page/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import welcome_pageTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,7 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class welcome_page(welcome_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
